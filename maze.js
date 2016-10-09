f154d2b1ba1d8b8d38af9040dc7fe808d8080b8f
zaavanr <zaavanrichards@hotmail.com>
2016-10-08 23:50:59 -0500

    exercise 2

--- maze.js
+++ maze.js
@@ -3,5 +3,12 @@
   		bound1.addEventListener('mouseover', function(){
   		bound1.setAttribute('class', ' boundary youlose');
   	})
+
+  		var allRed = document.querySelectorAll('.boundary');
+  		for(var i=0; i<allRed.length; i++){
+  			allRed[i].addEventListener('mouseover', function(){
+  			this.setAttribute('class', 'boundary youlose');
+  			})
+  		}
   	}
 
