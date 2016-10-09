9d4ce7d163e4d11ac2d909bf377d5c4ba5842767
zaavanr <zaavanrichards@hotmail.com>
2016-10-08 22:53:19 -0500

    exercise 1

---
+++ maze.js
@@ -0,0 +1,7 @@
+ 	window.onload= function(){
+  		var bound1= document.getElementById('boundary1');
+  		bound1.addEventListener('mouseover', function(){
+  		bound1.setAttribute('class', ' boundary youlose');
+  	})
+  	}
+
