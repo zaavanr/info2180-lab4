38e9fefe78c2d9dfea49834ea36c1b56fee6ce8d
zaavanr <zaavanrichards@hotmail.com>
2016-10-14 21:20:07 -0500

    Exercise 3

--- maze.js
+++ maze.js
@@ -2,31 +2,44 @@
  	window.onload= function(){
   		var bound1= document.getElementById('boundary1');
   		bound1.addEventListener('mouseover', function(){
-  		bound1.setAttribute('class', ' boundary youlose');
-  	})
-
-  		
-  		var allRed = document.querySelectorAll('.boundary');
-  		for(var i=0; i<allRed.length; i++){
-  			allRed[i].addEventListener('mouseover', function()
-  			{
-  				var allRed = document.querySelectorAll('.boundary');
-  				for(var i=0; i<allRed.length-1; i++){
-  					allRed[i].setAttribute('class', 'boundary youlose');
-  			
-  			}
-  			})
-  		}
-  		console.log(eCount);
-
-  		var winnings= document.getElementById('end');
-  		if(eCount > 0){
-  			alert('You Lose!');
-  		}
-  		else{
-  			winnings.addEventListener('mouseover', function(){
-  				alert('You Win!');
-  			})
-  		}
+  			bound1.setAttribute('class', ' boundary youlose');
+  		});
+  		ex3();
+  		ex2();
   	}
+
+  		function ex2(){
+  			var allRed = document.querySelectorAll('.boundary');
+  			for(var i=0; i<allRed.length; i++){
+  				allRed[i].addEventListener('mouseover', function()
+  				{
+
+  					var allRed = document.querySelectorAll('.boundary');
+  					for(var i=0; i<allRed.length-1; i++){
+  							allRed[i].setAttribute('class', 'boundary youlose');
+  						eCount++;			
+  					}
+
+  					if(eCount > 0)
+  						{
+  						alert('You Lose! :(');
+  						}
+  				});
+  			}
+  		}
+  		
+  		function ex3(){
+  			var winnings= document.getElementById('end');
+  			winnings.addEventListener('mouseover', function(){
+  				 	var response = document.getElementById("status");
+
+  					if(eCount === 0){
+  						alert('You Win!');
+  						}
+  				
+  				});
+
+  			}
+
+  	
 
